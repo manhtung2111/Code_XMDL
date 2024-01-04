51bbbb13ae3bb88096154b7fdb8e16a9ea5875bf
manhtung2111 <56856454+manhtung2111@users.noreply.github.com>
2024-01-04 10:57:10 +0700

    Update ftp_dl.js

--- ftp_dl.js
+++ ftp_dl.js
@@ -5,7 +5,7 @@ var _ = require('underscore');
 var path = require('path');
 var folder = '/home/linaro/data/';
 const SCHEDULE = "*/5 * * * *";
-const ftpclient = new ftp('103.126.153.235', 21, 'XIMANGDONGLAM', 'ximangdonglamhue', false)
+const ftpclient = new ftp('103.9.86.285', 21, 'capnuocsoctrang', 'BsmaK0Elcq', false)
 var schedule = require('node-schedule');
 function time(){
   var coeff = 1000*60*5;
@@ -22,7 +22,7 @@ function MostRecent(dir){
 function upl(){
   let fname = MostRecent(folder);
   let month = ("0" + (new Date().getMonth() + 1)).slice(-2);
-  let pathfile = './Test/ngthan/'+new Date().getFullYear()+ '/' + month+ '/' +  ("0" + new Date().getDate()).slice(-2) +'/';
+  let pathfile = './ST/{F63C96B5-01AD-40A4-895A-69BD8746B95F}/{D1627000-7B5E-468F-83F0-F4F35AC619E4}/'+new Date().getFullYear()+ '/' + month+ '/' +  ("0" + new Date().getDate()).slice(-2) +'/';
   ftpclient.upload(folder + fname ,pathfile, fname ,755);
   console.log(fname);
 }
