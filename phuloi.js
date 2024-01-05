14aa94ed67aee41f046bc9667127e775c070e101
manhtung2111 <56856454+manhtung2111@users.noreply.github.com>
2024-01-05 09:10:14 +0700

    phuloi

---
+++ phuloi.js
@@ -0,0 +1,30 @@
+const ftp = require('./upload');
+var fs = require ('fs');
+var schedule = require('node-schedule');
+var _ = require('underscore');
+var path = require('path');
+var folder = '/home/linaro/data/';
+const SCHEDULE = "*/5 * * * *";
+const ftpclient = new ftp('103.9.86.28', 21, 'capnuocsoctrang', 'BsmaK0Elcq', false)
+var schedule = require('node-schedule');
+function time(){
+  var coeff = 1000*60*5;
+  var rounded = new Date(Math.round((new Date().getTime() + 7*60*1000*60) / coeff) * coeff);
+  return rounded.toISOString(true).replace(/-|T|:/g,'').slice(0,14);
+}
+function MostRecent(dir){
+   var files = fs.readdirSync(dir);
+   return _.max(files, function (f){
+       var fullpath = path.join(dir,f);
+       return fs.statSync(fullpath).ctime;
+});
+}
+function upl(){
+  let fname = MostRecent(folder);
+  let month = ("0" + (new Date().getMonth() + 1)).slice(-2);
+  let pathfile = './ST//{F63C96B5-01AD-40A4-895A-69BD8746B95F}/{03EEA9E0-837C-470F-8C64-A9993C2D0BE6}/'+new Date().getFullYear()+ '/' + month+ '/' +  ("0" + new Date().getDate()).slice(-2) +'/';
+  ftpclient.upload(folder + fname ,pathfile, fname ,755);
+  console.log(fname);
+}
+
+upl();
